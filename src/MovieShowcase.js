d567233e8d0f00deea701ec4d2a2d865537fc74c
Ross Mawdsley <rossmawd@gmail.com>
2019-06-25 12:18:30 +0100

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js"; //the raw data
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+    // map over your movieData array and return the correct
+    return movieData.map(movie => {
+      return (
+        <MovieCard
+          title={movie.title}
+          IMDBRating={movie.IMDBRating}
+          genres={movie.genres}
+          poster={movie.poster}
+        />
+      );
+    });
+  };
 
   render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
 }
+
+//MovieCard compnents receive their individual movie information from MovieShowcase as four props: title, IMDBRating, genres, and poster
+// Following, the props are passed again to either CardFront or CardBack.
